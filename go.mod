6fc2a2650c8f96fb87a1bd19e5f7c22ed13296a4
sigumaa <66453922+Sigumaa@users.noreply.github.com>
2022-08-08 23:23:32 +0900

    feat: fuzzing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sigumaa/fuuzzz
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev, revErr := Reverse(input)
+	doubleRev, doubleRevErr := Reverse(rev)
+	fmt.Printf("original: %q\n", input)
+	fmt.Printf("reversed: %q, err: %v\n", rev, revErr)
+	fmt.Printf("reversed again: %q, err: %v\n", doubleRev, doubleRevErr)
+}
